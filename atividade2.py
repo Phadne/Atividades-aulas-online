eb6dcef82958a1af8e68545a035527e854adaef7
Daphne Torrelio <115555390+Phadne@users.noreply.github.com>
2022-12-16 00:22:41 -0300

    Revert "Create atividade2.py"
    This reverts commit 38460a452197f5713104bb862c088c6bacda5120.

--- atividade2.py
+++
@@ -1,7 +0,0 @@
-# Escreva um programa Python para somar todos os itens em uma lista
-def soma_list(items):
-    soma_numeros = 0
-    for x in items:
-        soma_numeros += x
-    return soma_numeros
-print(soma_list([90,6,-10]))
