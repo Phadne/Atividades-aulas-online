457b14d4b2ebeacdaba6075289831044d839cb0f
Daphne Torrelio <115555390+Phadne@users.noreply.github.com>
2022-12-16 00:24:31 -0300

    Create exemplo8.py

---
+++ exemplo8.py
@@ -0,0 +1,10 @@
+
+numero = int(input("Digite um numero: "))
+
+if (numero >= 1 and numero <= 9):
+
+ print("O valor está na faixa permitida.")
+
+else:
+
+ print("O valor está fora da faixa permitida.")
