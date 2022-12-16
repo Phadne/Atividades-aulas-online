bc310b90978d75614465cb113d60f6f59d8f7d89
Daphne Torrelio <115555390+Phadne@users.noreply.github.com>
2022-12-16 00:23:54 -0300

    Create exemplo6.py

---
+++ exemplo6.py
@@ -0,0 +1,10 @@
+#Multiplo de 10
+
+n = int(input("Até qual valor você gostaria de verificar? "))
+for i in range (1,n+1):
+  if ((i % 10 == 0) ):
+    print(i,"é múltiplo de 10")
+  elif (i % 10 == 0):
+    print(i,"é múltiplo de 10")
+  else:
+    print(i)
