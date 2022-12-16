83e2c761bd38ccc9bd5c9f5f147432c060d55e51
Daphne Torrelio <daphnetorrelio365@gmail.com>
2022-12-16 09:24:30 -0300

    Create exemplo9.py

---
+++ exemplo9.py
@@ -0,0 +1,13 @@
+# Armazenamento
+
+n = float(input("Digite um número para N: "))
+a = float(input("Digite um número para A: "))
+b = float(input("Digite um número para B: "))
+
+if (n>=0):
+    (a <- n)
+    print("O número: ",a,"é armazenado em A")
+
+else:
+    (b <- n)
+    print("O número: ",b,"é armazenado em B")
