914d0ff3678e4a7dfc0540975889acf7b92b1fd5
Daphne Torrelio <daphnetorrelio365@gmail.com>
2023-01-04 11:43:58 -0300

    Create ex-random.py

---
+++ RPGpokemon/ex-random.py
@@ -0,0 +1,48 @@
+# Crie um Pokemon baseado em uma lista de nomes, de forma aleatória
+
+# Gere o ataque, o hp e a defesa desse pokemon de forma aleatória, dependendo
+# da sua espécie.
+
+# Crie 2 pokemons e compare o mais forte.
+
+import random
+
+especiesPokemon = ("Pikachu","Diggypufle",)
+
+meuPokemon  = especiesPokemon[random.randint(0,len(especiesPokemon)-1)]
+
+match meuPokemon:
+    case "Pikachu":
+        meuPokemonAtaque = random.randint(30,50)
+        meuPokemonDefesa = random.randint(30,50)
+        meuPokemonHp = random.randint(30,50)
+
+match meuPokemon:
+    case "Pikachu":
+        meuPokemonAtaque = random.randint(30,50)
+        meuPokemonDefesa = random.randint(30,50)
+        meuPokemonHp = random.randint(30,50)
+
+    case "Pikachu":
+        meuPokemonAtaque = random.randint(30,50)
+        meuPokemonDefesa = random.randint(30,50)
+        meuPokemonHp = random.randint(30,50)
+
+    case "Pikachu":
+        meuPokemonAtaque = random.randint(30,50)
+        meuPokemonDefesa = random.randint(30,50)
+        meuPokemonHp = random.randint(30,50)
+
+    case "Bulbasaur":
+        meuPokemonAtaque = random.randint(30,50)
+        meuPokemonDefesa = random.randint(30,50)
+        meuPokemonHp = random.randint(30,50)
+
+pokemoninimigo = random.choice(PokemonEletrico)
+
+
+
+
+
+
+
